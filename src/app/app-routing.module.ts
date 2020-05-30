894d16cf5575a0d5a4d5308adee5c6da93557b98
Bruno Krebs <bruno.krebs@auth0.com>
2020-05-30 09:31:15 -0300

    first lazy loading modules;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
+  { path: 'products', loadChildren: () => import('./products/products.module').then((m) => m.ProductsModule) },
+  { path: 'inventory', loadChildren: () => import('./inventory/inventory.module').then((m) => m.InventoryModule) },
   // Fallback when no prior route is matched
   { path: '**', redirectTo: '', pathMatch: 'full' },
 ];
---
+++ src/app/products/products-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { extract } from '@app/i18n';
+import { ProductsComponent } from './products.component';
+import { Shell } from '@app/shell/shell.service';
+
+const routes: Routes = [
+  Shell.childRoutes([{ path: '', component: ProductsComponent, data: { title: extract('Products') } }]),
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
+})
+export class ProductsRoutingModule {}
