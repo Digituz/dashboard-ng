35da69f28c48f540747b384ab2c9642ef59f53c9
Bruno Krebs <bruno.krebs@auth0.com>
2020-05-29 22:09:59 -0300

    some basic template;

--- src/app/shell/shell.module.ts
+++ src/app/shell/shell.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TranslateModule } from '@ngx-translate/core';
 import { RouterModule } from '@angular/router';
+import { NzBreadCrumbModule } from 'ng-zorro-antd/breadcrumb';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { IconDefinition } from '@ant-design/icons-angular';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
 
 import { I18nModule } from '@app/i18n';
 import { ShellComponent } from './shell.component';
 import { HeaderComponent } from './header/header.component';
 
+import { AppstoreOutline, UserOutline, LaptopOutline, NotificationOutline } from '@ant-design/icons-angular/icons';
+
+const icons: IconDefinition[] = [AppstoreOutline, UserOutline, LaptopOutline, NotificationOutline];
+
 @NgModule({
-  imports: [CommonModule, TranslateModule, I18nModule, RouterModule],
+  imports: [
+    CommonModule,
+    TranslateModule,
+    I18nModule,
+    RouterModule,
+    NzLayoutModule,
+    NzBreadCrumbModule,
+    NzMenuModule,
+    NzIconModule.forRoot(icons),
+  ],
   declarations: [HeaderComponent, ShellComponent],
 })
 export class ShellModule {}
